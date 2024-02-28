b4171080e5f6b05af733a4f3d4fd348122b0479d
Mateus Ramos Pereira <mateusramospereira@hotmail.com>
2024-02-27 22:15:21 -0300

    "Imersão js - Aula 01"

---
+++ js/principal.js
@@ -0,0 +1,20 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var imc = peso / (altura ** 2);
+
+    tdImc.textContent = imc.toFixed(1);
+}
